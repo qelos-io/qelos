acb95893b6265ac665ca94ae849b37636ffbf716
David Meir-Levy <david@davidlevy.co.il>
2022-12-19 19:49:23 +0200

    docs intro

--- documentation/docs/.vitepress/config.ts
+++ documentation/docs/.vitepress/config.ts
@@ -1,22 +1,15 @@
 import {defineConfig} from 'vitepress'
 
 export default defineConfig({
-  title: 'Greenpress',
+  title: 'QELOS',
   description: '',
   outDir: '../dist',
   themeConfig: {
-    editLink: {
-      pattern: 'https://github.com/greenpress/greenpress/edit/main/documentation/docs/:path',
-      text: 'Edit this page on GitHub'
-    },
-    footer: {
-      message: 'Released under the MIT License.',
-    },
     sidebar: [
       {
         text: 'GETTING STARTED',
         items: [
-          {text: 'Intro to Greenpress', link: '/getting-started/intro'},
+          {text: 'Intro to QELOS', link: '/getting-started/intro'},
           {text: 'Installation', link: '/getting-started/installation'},
           {text: 'Deployment', link: '/getting-started/deployment'},
           {text: 'Create your first plugin', link: '/getting-started/create-your-first-plugin'},
