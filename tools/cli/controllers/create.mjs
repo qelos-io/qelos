912166536584b41b5416e8591eb9d86aa7b33505
David Meir-Levy <david@davidlevy.co.il>
2023-02-09 12:09:32 +0200

    apply custom versions

--- tools/cli/controllers/create.mjs
+++ tools/cli/controllers/create.mjs
@@ -6,6 +6,7 @@ import {join} from "path";
 import rimraf from 'rimraf'
 import ProgressBar from "../utils/progress-bar.mjs";
 import * as readline from "readline";
+import {execSync} from "child_process";
 
 const https = follow.https;
 
@@ -23,18 +24,50 @@ function askQuestion(question) {
   })
 }
 
-export default async function createController({name}) {
-  console.log(blue('Choose a boilerplate:'))
-  const project = await cliSelect({
-    values: {vanilla: 'Vanilla', react: 'React', vue: 'Vue', solid: 'Solid', custom: 'Custom from Github'},
-  })
-  let repository = project.id;
-  let organization = 'qelos-boilerplates';
+async function getOrgReposNames(organization) {
+  const res = await fetch(`https://api.github.com/users/${organization}/repos`)
+  const repos = await res.json();
 
-  if (repository === 'custom') {
-    const [githubOrg, githubRepo] = (await askQuestion('Enter custom github project (org/repo)\n')).split('/');
-    organization = githubOrg;
-    repository = githubRepo;
+  if (repos && repos instanceof Array) {
+    return repos.reduce((result, repo) => {
+      result[repo.name] = repo.description || repo.name;
+      return result;
+    }, {});
+  }
+  return [];
+}
+
+export default async function createController({name}) {
+  console.log(blue('Choose a boilerplate:'));
+  let organization = 'qelos-boilerplates';
+  let repository = 'vanilla';
+
+  try {
+    const project = await cliSelect({
+      values: {
+        vanilla: 'Vanilla',
+        react: 'React',
+        vue: 'Vue',
+        solid: 'Solid',
+        more: 'Show more',
+        custom: 'Custom from Github'
+      },
+    });
+    repository = project.id;
+
+    if (repository === 'more') {
+      const selected = await cliSelect({
+        values: await getOrgReposNames(organization)
+      });
+      repository = selected.id;
+    } else if (repository === 'custom') {
+      const [githubOrg, githubRepo] = (await askQuestion('Enter custom github project (org/repo)\n')).split('/');
+      organization = githubOrg || organization;
+      repository = githubRepo;
+    }
+  } catch {
+    console.log('See you soon :)');
+    process.exit(0);
   }
 
   const tempFolder = 'ql-plugin-' + Date.now();
@@ -67,6 +100,8 @@ export default async function createController({name}) {
         const rootFolder = await extract(unzipper);
 
         fs.renameSync(join(tempFolder, rootFolder), name);
+
+        execSync(`npm pkg set name=${name}`, {cwd: name});
 
         console.log(`Created ${name} successfully.`)
       } catch (err) {
