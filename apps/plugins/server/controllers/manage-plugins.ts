a68c6f773891448fdd1ffc7f34258d1b80d61d3d
David Meir-Levy <david@davidlevy.co.il>
2023-03-16 13:34:10 +0200

    bugfix: show mfes for regular users

--- apps/plugins/server/controllers/manage-plugins.ts
+++ apps/plugins/server/controllers/manage-plugins.ts
@@ -7,7 +7,7 @@ import logger from '../services/logger';
 import {isDev} from '../../config';
 
 export function getAllPlugins(req, res) {
-  const select = req.user?.hasPluginPrivileges ? '-token -auth' : 'name description microFrontends injectables navBarGroups'
+  const select = req.user?.hasPluginPrivileges ? '-token -auth' : 'name description callbackUrl microFrontends injectables navBarGroups'
   Plugin.find({tenant: req.headers.tenant}).select(select).lean().exec()
     .then(list => {
       res.json(list).end();
