74880af1fce77262c0b96db5028af3a8f0872a26
David Meir-Levy <david@davidlevy.co.il>
2024-06-15 17:13:51 +0300

    fix users list username display

--- apps/admin/src/modules/users/components/UsersList.vue
+++ apps/admin/src/modules/users/components/UsersList.vue
@@ -1,37 +1,32 @@
 <template>
   <GpItem v-for="user in users" :key="user._id">
     <template v-slot:title>
-      <router-link :to="{ name: 'editUser', params: { userId: user._id } }">{{
-        getUserFullName(user)
-      }}</router-link>
+      <router-link :to="{ name: 'editUser', params: { userId: user._id } }">{{ getUserFullName(user) }}</router-link>
     </template>
     <div class="metadata">
-      <p v-if="user.email">
-        {{ $t("Email") }}:
-        <a :href="'mailto:' + user.email" @click.stop>{{ user.email }}</a>
-      </p>
-      <p>{{ $t("Roles") }}: {{ join(user.roles) }}</p>
+      <p v-if="user.username">{{ $t('Username') }}: {{ user.username }}</p>
+      <p>{{ $t('Roles') }}: {{ join(user.roles) }}</p>
     </div>
     <template v-slot:actions>
       <a @click.prevent="remove(user)">
         <el-icon>
-          <icon-delete />
+          <icon-delete/>
         </el-icon>
-        {{ $t("Remove") }}
+        {{ $t('Remove') }}
       </a>
     </template>
   </GpItem>
 </template>
 <script lang="ts" setup>
-import { useUsersList, useRemoveUser } from "../compositions/users";
-import GpItem from "@/modules/core/components/layout/BlockItem.vue";
+import { useUsersList, useRemoveUser } from '../compositions/users';
+import GpItem from '@/modules/core/components/layout/BlockItem.vue';
 
 const { users } = useUsersList();
 const { remove } = useRemoveUser((id) => {
   users.value = users.value.filter((user) => user._id !== id);
 });
 
-const join = (arr: string[]) => arr.join(", ");
+const join = (arr: string[]) => arr.join(', ');
 
 function getUserFullName(user) {
   let fullName = user.name || user.fullName;
@@ -39,9 +34,9 @@ function getUserFullName(user) {
     return fullName;
   }
   if (user.firstName) {
-    return user.firstName + (user.lastName ? " " + user.lastName : "");
+    return user.firstName + (user.lastName ? ' ' + user.lastName : '');
   }
-  return "Unknown";
+  return 'Unknown';
 }
 </script>
 <style scoped>
