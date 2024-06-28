5cf2d6f67cd512293d24defae9bd102a5319706c
David Meir-Levy <david@davidlevy.co.il>
2024-06-28 12:57:32 +0300

    fix sdk ts build

---
+++ packages/global-types/index.js
@@ -0,0 +1,8 @@
+export * from './blueprint';
+export * from './injectable';
+export * from './event-subscriber';
+export * from './plugin';
+export * from './micro-frontend';
+export * from './navbar-group';
+export * from './plugin-crud';
+//# sourceMappingURL=index.js.map
