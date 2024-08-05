d737cd404f9681e101cf7dc38f40043f0cce6764
David Meir-Levy <david@davidlevy.co.il>
2024-08-05 11:09:59 +0300

    validate duplicate websites urls

--- apps/content/helpers/host.js
+++ apps/content/helpers/host.js
@@ -12,6 +12,13 @@ const HOST = process.env.HOST || "127.0.0.1";
 const Configuration = mongoose.model("Configuration");
 
 async function changeHost() {
+
+  const otherTenantWithHostname = await Configuration.findOne({ 'metadata.websitesUrls': HOST }).exec()
+
+  if (otherTenantWithHostname) {
+    throw new Error(`The hostname ${HOST} is already used by another tenant`);
+  }
+
   const configuration = await Configuration.findOne({ tenant: TENANT, key: appConfiguration }).exec()
 
   configuration.metadata.websiteUrls = [HOST];
@@ -19,6 +26,7 @@ async function changeHost() {
   configuration.markModified('metadata.websiteUrls');
   await configuration.save()
 }
+
 console.log("initiate content");
 
 Promise.all([
