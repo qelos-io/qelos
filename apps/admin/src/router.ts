b3f472947a9a16a34d85c52024d1297fc055bb6b
David Meir-Levy <david@davidlevy.co.il>
2024-08-26 23:30:16 +0300

    page title

--- apps/admin/src/router.ts
+++ apps/admin/src/router.ts
@@ -1,16 +1,17 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Authenticated from './modules/core/Authenticated.vue'
 import Home from './modules/core/Home.vue'
 import assetsRoutes from './modules/assets/routes'
 import usersRoutes from './modules/users/routes'
 import configurationsRoutes from './modules/configurations/routes'
-import {authStore, fetchAuthUser} from './modules/core/store/auth'
+import { authStore, fetchAuthUser } from './modules/core/store/auth'
 import draftsRoutes from './modules/drafts/routes';
 import blocksRoutes from './modules/blocks/routes';
 import layoutsRoutes from '@/modules/layouts/routes';
-import {managePluginsRoutes, playRoutes} from '@/modules/plugins/routes';
+import { managePluginsRoutes, playRoutes } from '@/modules/plugins/routes';
 import workspacesRoutes from '@/modules/workspaces/routes';
 import noCodeRoutes from '@/modules/no-code/routes';
+import { useAppConfiguration } from '@/modules/configurations/store/app-configuration';
 
 // @ts-ignore
 const BASE = import.meta.env.BASE_URL || '/';
@@ -71,6 +72,15 @@ router.beforeEach(async (to, from, next) => {
   next({
     name: 'login'
   })
+})
+
+router.afterEach((to) => {
+  const { appConfig } = useAppConfiguration()
+  if (to.meta?.name) {
+    document.querySelector('title').innerHTML = `${to.meta.name} | ${appConfig.value.name}`;
+  } else {
+    document.querySelector('title').innerHTML = appConfig.value.name;
+  }
 })
 
 export default router
