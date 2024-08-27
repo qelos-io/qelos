f099c6411bb7c17383b4d8656120f15278948397
Volodymyr Moroz <navigators.ua@gmail.com>
2024-08-27 08:20:10 +0000

    86c0304nz filter user by role admin

--- apps/admin/src/modules/core/components/semantics/ListPageTitle.vue
+++ apps/admin/src/modules/core/components/semantics/ListPageTitle.vue
@@ -1,58 +1,51 @@
 <template>
-  <h1>
-    <RemoveButton v-if="editableManager"
-                  class="remove-component-btn"
-                  @click="editableManager.removeComponent($el)" />
+  <h1 class="title-container">
+    <RemoveButton v-if="editableManager" class="remove-component-btn" @click="editableManager.removeComponent($el)" />
     <span>{{ $t(title) }}</span>
-    <el-button text v-if="createRoutePath || createRoute || onCreate" @click="create">
+    <slot name="content"></slot>
+    <el-button text v-if="createRoutePath || createRoute || onCreate" @click="create" class="add-button">
       <el-icon size="28">
         <font-awesome-icon :icon="['far', 'square-plus']" />
       </el-icon>
     </el-button>
   </h1>
 </template>
+
 <script lang="ts" setup>
 import { useRouter } from 'vue-router';
 import { inject } from 'vue';
 import RemoveButton from '@/modules/core/components/forms/RemoveButton.vue';
 
-const router = useRouter()
-const emit = defineEmits(['create', 'removeComponent'])
+const router = useRouter();
+const emit = defineEmits(['create', 'removeComponent']);
 
 const props = defineProps({
   title: String,
   createRoute: String,
   createRoutePath: String,
   onCreate: Function
-})
+});
 
 function create() {
   if (props.onCreate) {
-    emit('create')
+    emit('create');
   } else {
-    router.push(props.createRoutePath || { name: props.createRoute })
+    router.push(props.createRoutePath || { name: props.createRoute });
   }
 }
-
 
 const editableManager = inject('editableManager');
 </script>
 
 <style scoped lang="scss">
-h1 {
-  position: sticky;
-  top: 0;
-  z-index: 1;
-  background-color: var(--body-bg);
+.title-container {
   display: flex;
-  justify-content: center;
   align-items: center;
-  span {
-    margin-inline-end: auto;
-  }
+  width: 100%;
 
-  > * {
-    vertical-align: middle;
-  }
+}
+
+.add-button {
+  margin-left: auto;
 }
 </style>
--- apps/admin/src/modules/users/compositions/users.ts
+++ apps/admin/src/modules/users/compositions/users.ts
@@ -2,9 +2,9 @@ import { useConfirmAction } from "../../core/compositions/confirm-action";
 import usersService from "../../../services/users-service";
 import { useDispatcher } from "../../core/compositions/dispatcher";
 import { useSubmitting } from "../../core/compositions/submitting";
-import { useRoute } from "vue-router";
-import { Ref, watch } from 'vue';
-import {IUser} from '@/modules/core/store/types/user';
+import { useRoute, useRouter } from "vue-router";
+import { computed, Ref, watch } from 'vue';
+import { IUser } from '@/modules/core/store/types/user';
 
 export function useEditUsers(userId) {
   const { result: user } = useDispatcher<IUser>(() => usersService.getOne(userId));
@@ -48,14 +48,42 @@ export function useRemoveUser(onSuccess) {
   };
 }
 
-export function useUsersList(): { users: Ref<Array<IUser>>; loading: Ref<boolean> } {
+export function useUsersList(): { users: Ref<Array<IUser>>; filteredUsers: Ref<Array<IUser>>; loading: Ref<boolean>; updateRoles: (newRoles: string[]) => void } {
   const route = useRoute();
+  const router = useRouter();
+
+  // Reading role parameters from query
+  const roles = computed(() => route.query.roles ? (route.query.roles as string).split(',') : []);
+
+  // Function to update query parameters
+  function updateRoles(newRoles: string[]) {
+    router.push({
+      query: {
+        ...route.query,
+        roles: newRoles.length ? newRoles.join(',') : undefined
+      }
+    });
+  }
 
   const { result: users, retry, loading } = useDispatcher<IUser[]>(
-    () => usersService.getAll({ username: route.query.q }),
+    () => usersService.getAll({
+      username: route.query.q,
+      roles: roles.value.length ? roles.value.join(',') : undefined,
+    }),
     []
   );
-  watch(() => route.query.q, retry);
 
-  return { users, loading };
+  // Computed property to filter users based on selected roles
+  const filteredUsers = computed(() => {
+    if (roles.value.length === 0) return users.value;
+    return users.value.filter(user =>
+      user.roles.some(role => roles.value.includes(role))
+    );
+  });
+
+  // Watch for changes in query parameters `q` and `roles`
+  watch([() => route.query.q, roles], retry);
+
+  return { users, filteredUsers, loading, updateRoles };
 }
+
