6e77adb422b9972268ce2ad6e2b3127cbe41a1aa
David Meir-Levy <david@davidlevy.co.il>
2024-09-02 11:52:47 +0300

    steps

--- apps/admin/components.d.ts
+++ apps/admin/components.d.ts
@@ -29,6 +29,8 @@ declare module 'vue' {
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
     ElOption: typeof import('element-plus/es')['ElOption']
     ElSelect: typeof import('element-plus/es')['ElSelect']
+    ElStep: typeof import('element-plus/es')['ElStep']
+    ElSteps: typeof import('element-plus/es')['ElSteps']
     ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
     ElSwitch: typeof import('element-plus/es')['ElSwitch']
     ElTable: typeof import('element-plus/es')['ElTable']
