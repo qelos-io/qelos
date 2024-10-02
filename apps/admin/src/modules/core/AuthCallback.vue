ecf49503594af3ae8a4dcb940f70a28e7123181d
David Meir-Levy <david@davidlevy.co.il>
2024-10-02 14:35:17 +0300

    minor fix to auth callback

--- apps/admin/src/modules/core/AuthCallback.vue
+++ apps/admin/src/modules/core/AuthCallback.vue
@@ -1,30 +1,25 @@
 <script setup lang="ts">
-import { useRoute, useRouter } from 'vue-router';
 import { ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { api } from '@/services/api';
 
 const route = useRoute();
 const router = useRouter();
 const response = ref('');
 
-async function convertRefreshToken(refreshToken: string, returnUrl: string) {
-	try {
-		const res = await fetch(`/api/auth/callback?rt=${refreshToken}`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			credentials: 'include',
-		});
+async function callbackFromOAuth(refreshToken: string, returnUrl: string) {
+  try {
+    const res = await api.post(`/api/auth/callback?rt=${refreshToken}`);
 
-		if (res.ok) {
-			// After a successful request, redirect to returnUrl
-			await router.push(returnUrl || '/');
-		} else {
-			response.value = 'Error processing refresh token';
-		}
-	} catch (error) {
-		response.value = `Error: ${error.message}`;
-	}
+    if (res.status === 200) {
+      // After a successful request, redirect to returnUrl
+      await router.push(returnUrl || '/');
+    } else {
+      response.value = 'Error processing refresh token';
+    }
+  } catch (error) {
+    response.value = `Error: ${error.message}`;
+  }
 }
 
 // Get parameters from query
@@ -32,16 +27,14 @@ const refreshToken = route.query.rt as string || '';
 const returnUrl = route.query.returnUrl as string || '/';
 
 if (refreshToken) {
-	// Token conversion
-	convertRefreshToken(refreshToken, returnUrl);
+  // Token conversion
+  callbackFromOAuth(refreshToken, returnUrl);
 } else {
-	response.value = 'No refresh token found';
+  response.value = 'No refresh token found';
 }
 </script>
 
 <template>
-	<div>
-		<p v-if="response">{{ response }}</p>
-		<p v-else>Processing...</p>
-	</div>
+  <p v-if="response">{{ response }}</p>
+  <p v-else>Processing...</p>
 </template>
