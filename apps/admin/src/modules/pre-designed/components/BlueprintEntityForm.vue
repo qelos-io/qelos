fd9b8f5b7502bc54b35eed7e31d723474f8243d0
David Meir-Levy <david@davidlevy.co.il>
2024-11-10 22:36:20 +0200

    ability to navigate to another page after submit

--- apps/admin/src/modules/pre-designed/components/BlueprintEntityForm.vue
+++ apps/admin/src/modules/pre-designed/components/BlueprintEntityForm.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
 import GeneralForm from '@/modules/pre-designed/components/GeneralForm.vue';
 import sdk from '@/services/sdk';
 import { useScreenRequirementsStore } from '@/modules/pre-designed/compositions/screen-requirements';
+
+const router = useRouter();
 
 const props = defineProps<{
   blueprint: string,
@@ -9,15 +12,28 @@ const props = defineProps<{
   successMsg?: string;
   errorMsg?: string;
   clearAfterSubmit?: boolean;
+  navigateAfterSubmit?: string;
 }>()
 
-const onSubmit = (form) => sdk.blueprints.entitiesOf(props.blueprint).create({metadata: form})
+const onSubmit = (form) => sdk.blueprints.entitiesOf(props.blueprint).create({ metadata: form })
 
 const { reloadBlueprintRequirements } = useScreenRequirementsStore();
+
+function afterSubmit(result: any) {
+  reloadBlueprintRequirements(props.blueprint);
+  if (props.navigateAfterSubmit) {
+    router.push({
+      name: props.navigateAfterSubmit,
+      params: {
+        identifier: result.identifier
+      }
+    })
+  }
+}
 </script>
 
 <template>
-  <GeneralForm :on-submit="onSubmit" @submitted="reloadBlueprintRequirements(props.blueprint)" v-bind="props">
+  <GeneralForm :on-submit="onSubmit" @submitted="afterSubmit" v-bind="props">
     <template #default="context">
       <slot v-bind="context"/>
     </template>
