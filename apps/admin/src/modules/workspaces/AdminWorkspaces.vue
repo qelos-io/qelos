fdf11937ee54dae9e14fa15c4ab08eca2cc25ad6
Volodymyr Moroz <navigators.ua@gmail.com>
2024-11-27 14:32:40 +0000

    86c13fk0u filter workspaces by labels frontend

--- apps/admin/src/modules/workspaces/AdminWorkspaces.vue
+++ apps/admin/src/modules/workspaces/AdminWorkspaces.vue
@@ -1,23 +1,58 @@
 <template>
-	<div class="menus-page">
-		<ListPageTitle title="Workspaces" :create-route="canUserCreateWorkspace ? 'adminCreateWorkspace' : undefined" />
-		<InvitesList v-if="store.invites.length" />
-		<AdminWorkspacesList />
-
-	</div>
+  <div class="menus-page">
+    <div class="header-container">
+      <ListPageTitle title="Workspaces" :create-route="canUserCreateWorkspace ? 'adminCreateWorkspace' : undefined">
+        <template v-slot:content>
+          <div class="filter-container">
+            <WorkspaceLabelFilter v-model="selectedLabels" :availableLabels="availableLabels" />
+          </div>
+        </template>
+      </ListPageTitle>
+    </div>
+    <AdminWorkspacesList :selected-labels="selectedLabels" />
+  </div>
 </template>
-<script lang="ts" setup>
-import { toRefs } from 'vue';
-import ListPageTitle from '@/modules/core/components/semantics/ListPageTitle.vue'
 
-import InvitesList from '@/modules/workspaces/components/InvitesList.vue';
-import useInvitesList from '@/modules/workspaces/store/invites-list';
+<script lang="ts" setup>
+import { computed, ref } from 'vue';
+import ListPageTitle from '@/modules/core/components/semantics/ListPageTitle.vue';
 import { useWsConfiguration } from '@/modules/configurations/store/ws-configuration';
 import AdminWorkspacesList from './components/AdminWorkspacesList.vue';
+import WorkspaceLabelFilter from './components/WorkspaceLabelFilter.vue';
+import { useRoute, useRouter } from 'vue-router';
 
+const route = useRoute()
+const router = useRouter()
 
-const store = useInvitesList()
+// State variables
+const availableLabels = ref<string[]>(['*', 'supplier', 'store', 'consumer']);
 
-const { canUserCreateWorkspace } = toRefs(useWsConfiguration())
+const selectedLabels = computed({
+  get: () => {
+    // Get labels from query
+    const labels = route.query.labels;
+    return Array.isArray(labels) ? labels : labels ? [labels] : [];
+  },
+  set: (newLabels) => {
+    // Update query route parameters
+    router.push({
+      query: { ...route.query, labels: newLabels.length ? newLabels : undefined },
+    });
+  },
+});
 
+const { canUserCreateWorkspace } = useWsConfiguration();
 </script>
+
+<style scoped>
+.header-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.filter-container {
+  margin-left: 50px;
+  height: 32px;
+}
+</style>
--- apps/admin/src/modules/workspaces/components/AdminWorkspacesList.vue
+++ apps/admin/src/modules/workspaces/components/AdminWorkspacesList.vue
@@ -1,40 +1,64 @@
-<template>
-	<div>
-		<BlockItem v-for="workspace in filteredWorkspaces" :key="workspace._id">
-			<template v-slot:title>
-				<router-link :to="{ name: 'editMyWorkspace', params: { id: workspace._id } }">
-					{{ workspace.name }}
-				</router-link>
-			</template>
-			<template v-slot:actions>
-				<el-button text type="danger" @click.prevent="store.remove(workspace._id)">
-					<el-icon>
-						<icon-delete />
-					</el-icon>
-					<span>Remove</span>
-				</el-button>
-			</template>
-		</BlockItem>
-	</div>
-</template>
-
 <script lang="ts" setup>
-
-import BlockItem from '@/modules/core/components/layout/BlockItem.vue';
+import { computed } from 'vue';
 import useAdminWorkspacesList from '../store/admin-workspaces-list';
 import { useRoute } from 'vue-router';
-import { computed } from 'vue';
 
 const store = useAdminWorkspacesList();
+const route = useRoute();
 
-const route = useRoute()
+const props = defineProps<{
+  selectedLabels: string[];
+}>();
 
 const filteredWorkspaces = computed(() => {
-	const reg = new RegExp(route.query.q?.toString(), 'i');
+  const reg = new RegExp(route.query.q?.toString(), 'i');
 
-	return store.workspaces.filter(workspace => {
-		return reg.test(workspace.name)
-	})
-})
+  return store.workspaces.filter(workspace => {
+    const matchesQuery = reg.test(workspace.name);
+    const matchesLabels =
+      props.selectedLabels.length === 0 || props.selectedLabels.every(label => workspace.labels.includes(label));
 
+    return matchesQuery && matchesLabels;
+  });
+});
 </script>
+
+<template>
+  <div>
+    <BlockItem v-for="workspace in filteredWorkspaces" :key="workspace._id">
+      <template v-slot:title>
+        <router-link :to="{ name: 'editMyWorkspace', params: { id: workspace._id } }">
+          {{ workspace.name }}
+        </router-link>
+      </template>
+      <template v-slot:default>
+        <div v-if="workspace.labels && workspace.labels.length" class="labels-container">
+          <span v-for="label in workspace.labels" :key="label" class="label-item">{{ label }}</span>
+        </div>
+      </template>
+      <template v-slot:actions>
+        <el-button text type="danger" @click.prevent="store.remove(workspace._id)">
+          <el-icon>
+            <icon-delete />
+          </el-icon>
+          <span>{{ $t('Remove') }}</span>
+        </el-button>
+      </template>
+    </BlockItem>
+  </div>
+</template>
+
+<style scoped>
+.labels-container {
+  margin-top: 8px;
+}
+
+.label-item {
+  display: inline-block;
+  margin-right: 5px;
+  padding: 4px 8px;
+  background-color: #f0f0f0;
+  border-radius: 4px;
+  font-size: 0.9em;
+}
+</style>
