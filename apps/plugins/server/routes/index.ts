5f0c68db8362cf0224482e517d44cbfdd19b248b
David Meir-Levy <david@davidlevy.co.il>
2025-01-06 10:02:38 +0200

    fix integrations bug

--- apps/plugins/server/routes/index.ts
+++ apps/plugins/server/routes/index.ts
@@ -2,14 +2,14 @@ import { app as getApp } from '@qelos/api-kit'
 import { managePlugins } from './manage-plugins';
 import { playPlugins } from './play-plugins';
 import eventsRouter from './events';
-// import { integrationsRouter } from './integrations';
-// import { integrationSourcesRouter } from './integration-sources';
+import { integrationsRouter } from './integrations';
+import { integrationSourcesRouter } from './integration-sources';
 
 export async function loadRoutes() {
   const app = getApp()
   app.use(managePlugins());
   app.use(playPlugins());
   app.use(eventsRouter);
-  // app.use(integrationSourcesRouter());
-  // app.use(integrationsRouter());
+  app.use(integrationSourcesRouter());
+  app.use(integrationsRouter());
 }
