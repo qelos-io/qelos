426ae0937d5a69f867eb44e1ee7b4460701e2c45
David Meir-Levy <david@davidlevy.co.il>
2025-01-07 23:03:27 +0200

    workaround after social login success but not routing home

--- apps/admin/src/modules/core/AuthCallback.vue
+++ apps/admin/src/modules/core/AuthCallback.vue
@@ -1,19 +1,25 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
+import { onMounted, ref } from 'vue';
+import { RouterLink, useRoute, useRouter } from 'vue-router';
 import { api } from '@/services/api';
 
 const route = useRoute();
 const router = useRouter();
 const response = ref('');
+const hasToken = ref(false);
+
+const refreshToken = route.query.rt as string || '';
+  const returnUrl = route.query.returnUrl as string || '/';
+
 
 async function callbackFromOAuth(refreshToken: string, returnUrl: string) {
   try {
     const res = await api.post(`/api/auth/callback?rt=${refreshToken}`);
 
     if (res.status === 200) {
+      hasToken.value = true;
       // After a successful request, redirect to returnUrl
-      await router.push(returnUrl || '/');
+      router.push(returnUrl || '/');
     } else {
       response.value = 'Error processing refresh token';
     }
@@ -22,19 +28,22 @@ async function callbackFromOAuth(refreshToken: string, returnUrl: string) {
   }
 }
 
-// Get parameters from query
-const refreshToken = route.query.rt as string || '';
-const returnUrl = route.query.returnUrl as string || '/';
-
-if (refreshToken) {
-  // Token conversion
-  callbackFromOAuth(refreshToken, returnUrl);
-} else {
-  response.value = 'No refresh token found';
-}
+onMounted(() => {
+  // Get parameters from query
+  
+  if (refreshToken) {
+    // Token conversion
+    callbackFromOAuth(refreshToken, returnUrl);
+  } else {
+    response.value = 'No refresh token found';
+  }
+})
 </script>
 
 <template>
   <p v-if="response">{{ response }}</p>
+  <p v-if="hasToken">
+    <router-link :to="returnUrl">{{ $t('Go to home') }}</router-link>
+  </p>
   <p v-else>Processing...</p>
 </template>
