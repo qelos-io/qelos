d6ee3caa454ae21dfe2ef4d5e3f6eb919e23f93a
David Meir-Levy <david@davidlevy.co.il>
2025-03-03 15:34:55 +0200

    more palettes

--- apps/admin/src/modules/configurations/components/DesignConfiguration.vue
+++ apps/admin/src/modules/configurations/components/DesignConfiguration.vue
@@ -6,6 +6,7 @@ import ColorsPaletteInput from '@/modules/configurations/components/ColorsPalett
 import { useLiveEditStore } from '@/modules/layouts/store/live-edit';
 import FormInput from '@/modules/core/components/forms/FormInput.vue';
 import configurationsService from '@/services/configurations-service';
+import FormRowGroup from '@/modules/core/components/forms/FormRowGroup.vue';
 
 const { appConfig } = useAppConfiguration();
 
@@ -31,14 +32,18 @@ const updateColors = debounce(changePalette, 660);
 
 async function updateUrlProperty(key: string, url: string) {
   if (url.trim() === '' || url.startsWith('http')) {
-    appConfig.value[key] = url.trim();
-    await configurationsService.update('app-configuration', {
-      metadata: {
-        ...appConfig.value,
-      }
-    })
-    await softResetConfiguration();
+    await updateProperty(key, url.trim())
   }
+}
+
+async function updateProperty(key: string, value: string | number) {
+  appConfig.value[key] = value;
+  await configurationsService.update('app-configuration', {
+    metadata: {
+      ...appConfig.value,
+    }
+  })
+  await softResetConfiguration();
 }
 </script>
 
@@ -46,7 +51,21 @@ async function updateUrlProperty(key: string, url: string) {
   <div>
     <ColorsPaletteInput v-model="colors" @update:modelValue="updateColors"/>
   </div>
-  <div class="container">
+  <FormRowGroup>
+    <div class="flex-row">
+      <el-form-item :label="$t('Border Radius')" class="flex-1">
+        <el-slider v-model="appConfig.borderRadius" :step="1" :min="0" :max="20" show-stops @change="updateProperty('borderRadius', appConfig.borderRadius)"/>
+      </el-form-item>
+      <div class="border-radius-demo" :style="{borderRadius: appConfig.borderRadius + 'px'}"></div>
+    </div>
+    <div class="flex-row">
+      <el-form-item :label="$t('Base Font Size')" class="flex-1">
+        <el-slider v-model="appConfig.baseFontSize" :step="1" :min="8" :max="28" show-stops @change="updateProperty('baseFontSize', appConfig.baseFontSize)"/>
+      </el-form-item>
+      <div class="font-size-demo" :style="{fontSize: appConfig.baseFontSize + 'px'}">ABC</div>
+    </div>
+  </FormRowGroup>
+  <FormRowGroup class="container">
     <FormInput v-model="appConfig.themeStylesUrl"
                placeholder="https://.."
                type="url"
@@ -61,9 +80,23 @@ async function updateUrlProperty(key: string, url: string) {
                :label="$t('URL to a Javascript file')"
                @keyup.enter="updateUrlProperty('scriptUrl', appConfig.scriptUrl)"
     />
-  </div>
+  </FormRowGroup>
 </template>
 
 <style scoped>
 
+.border-radius-demo {
+  width: 30px;
+  height: 30px;
+  border: 1px solid var(--border-color);
+  background-color: var(--main-color-light);
+  align-self: center;
+  margin-block-start: 10px;
+}
+
+.font-size-demo {
+  height: 30px;
+  align-self: center;
+  padding: 10px;
+}
 </style>
