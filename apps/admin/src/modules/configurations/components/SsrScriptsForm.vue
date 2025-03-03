6e6609526ea23553e174f6c49e56a4e7c4ca6728
David Meir-Levy <david@davidlevy.co.il>
2025-03-03 14:50:00 +0200

    ssr config improve

--- apps/admin/src/modules/configurations/components/SsrScriptsForm.vue
+++ apps/admin/src/modules/configurations/components/SsrScriptsForm.vue
@@ -1,5 +1,8 @@
 <template>
   <el-form @submit.native.prevent="save" class="ssr-scripts-form">
+    <p>Feel free to use the following variables:<br>
+      <el-tag class="tag" v-for="key in keys" :key="key" v-html="getKeyTemplate(key)" @click="copyToClipboard(key)"/>
+    </p>
     <el-form-item label="Enter head">
       <Monaco v-model="edited.head"
               language="html"/>
@@ -9,16 +12,24 @@
               language="html"/>
     </el-form-item>
 
-    <SaveButton :submitting="submitting"/>
+    <div class="submit-wrapper">
+      <SaveButton :submitting="submitting"/>
+    </div>
   </el-form>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { ElMessage } from 'element-plus';
 import Monaco from '../../users/components/Monaco.vue'
 import SaveButton from '@/modules/core/components/forms/SaveButton.vue';
 import { clearNulls } from '../../core/utils/clear-nulls';
-import { useEditMetadata } from '../compositions/metadata';
-import { ref } from 'vue';
+
+const { t } = useI18n()
+
+
+const keys = [ 'name', 'logoUrl', 'description', 'keywords', 'slogan', 'language', 'direction', 'themeStylesUrl', 'scriptUrl', 'homeScreen', 'borderRadius', 'baseFontSize' ]
 
 const props = defineProps({
   kind: String,
@@ -26,9 +37,29 @@ const props = defineProps({
   submitting: Boolean
 })
 
+function getKeyTemplate(key: string) {
+  return `{{${key}}}`
+}
+
+
+function copyToClipboard(key: string) {
+  navigator.clipboard.writeText(getKeyTemplate(key));
+  ElMessage.info({
+    message: t('Copied to clipboard'),
+    type: 'info',
+    center: true,
+    customClass: 'copy-to-clipboard-message'
+  })
+}
 
 const defaultMetadata = {
-  head: `<link rel="icon" href="/favicon.ico" /><title>APP</title>`,
+  head: `<link rel="icon" href="/favicon.ico" /><title>{{name}} - {{slogan}}</title>
+<meta property="og:title" content="{{name}} - {{slogan}}">
+<meta property="og:description" content="{{description}}">
+<meta property="og:image" content="{{logoUrl}}">
+<meta property="og:type" content="webapp">
+<meta property="og:site_name" content="{{name}}">
+<meta property="og:locale" content="{{language}}">`,
   body: ''
 }
 const edited = ref({
@@ -42,4 +73,18 @@ function save() {
   emit('save', clearNulls(edited.value))
 }
 </script>
+<style scoped>
+.tag {
+  margin-inline: 5px;
+  cursor: pointer;
+}
+.submit-wrapper {
+  z-index: 5;
+  background-color: var(--body-bg);
+  margin: 0;
+  padding: 20px;
+  position: sticky;
+  bottom: 0;
+}
+</style>
 
