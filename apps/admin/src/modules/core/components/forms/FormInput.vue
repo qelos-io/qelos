e8311810cd56a96226347d802589406bef24bedd
David Meir-Levy <david@davidlevy.co.il>
2025-03-15 01:12:02 +0200

    hidden input

--- apps/admin/src/modules/core/components/forms/FormInput.vue
+++ apps/admin/src/modules/core/components/forms/FormInput.vue
@@ -1,11 +1,12 @@
 <template>
-  <div>
-    <EditComponentBar/>
+  <div v-if="type !== 'hidden'">
+    <EditComponentBar v-if="isEditingEnabled"/>
     <el-form-item :label="title ? $t(title) : null" :required="required">
       <small v-if="label"> ({{ $t(label) }})</small>
       <small v-else-if="gap">&nbsp;</small>
       <slot name="pre"/>
-      <el-input-number v-if="type === 'number'" v-on="listeners" :size="size || 'large'" :model-value="modelValue as number"/>
+      <el-input-number v-if="type === 'number'" v-on="listeners" :size="size || 'large'"
+                       :model-value="modelValue as number"/>
       <el-switch
           v-else-if="type === 'switch'"
           v-model="modelValue as boolean"
@@ -32,7 +33,8 @@
         </template>
         <slot name="options"/>
       </el-select>
-      <AssetUploader v-else-if="type === 'upload' || type === 'file'" v-on="listeners" :value="modelValue" @change="$emit('update:modelValue', $event)" class="asset-upload"/>
+      <AssetUploader v-else-if="type === 'upload' || type === 'file'" v-on="listeners" :value="modelValue"
+                     @change="$emit('update:modelValue', $event)" class="asset-upload"/>
       <el-input v-else v-on="listeners" :model-value="modelValue as (string | number)" :placeholder="placeholder"
                 :size="size || 'large'"
                 :native-type="type"
@@ -42,19 +44,29 @@
     </el-form-item>
     <slot/>
   </div>
+  <div v-else-if="isEditingEnabled">
+    <EditComponentBar/>
+  </div>
 </template>
 
 <script lang="ts">
+import { watch } from 'vue';
 import AssetUploader from '@/modules/assets/components/AssetUploader.vue'
 import EditComponentBar from '@/modules/no-code/components/EditComponentBar.vue';
+import { isEditingEnabled } from '@/modules/core/store/auth';
 
 export default {
   name: 'FormInput',
+  computed: {
+    isEditingEnabled() {
+      return isEditingEnabled
+    }
+  },
   components: { EditComponentBar, AssetUploader },
   props: {
     title: String,
     label: String,
-    type: String as () => 'text' | 'textarea' | 'password' | 'button' | 'checkbox' | 'file' | 'email' | 'number' | 'radio' | 'upload' | 'switch' | 'color' | 'url' | 'select',
+    type: String as () => 'text' | 'textarea' | 'password' | 'button' | 'checkbox' | 'file' | 'email' | 'number' | 'radio' | 'upload' | 'switch' | 'color' | 'url' | 'select' | 'hidden',
     placeholder: String,
     gap: Boolean,
     size: String as () => 'large' | 'default' | 'small',
@@ -68,9 +80,15 @@ export default {
       type: Object,
       default: () => ({})
     },
+    value: [String, Number, Object, Boolean],
   },
   emits: ['input', 'change', 'update:modelValue'],
-  setup(_, { emit }) {
+  setup(props, { emit }) {
+    if (props.type === 'hidden') {
+      watch(() => props.value, () => {
+        emit('update:modelValue', props.value);
+      }, { immediate: true })
+    }
     return {
       listeners: {
         input: (event) => {
