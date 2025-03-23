4e2cc65ba5307c02645d73b10ec2ee0dfb2a6005
David Meir-Levy <david@davidlevy.co.il>
2025-03-23 08:43:20 +0200

    force home screen when exists

--- apps/admin/src/modules/core/Home.vue
+++ apps/admin/src/modules/core/Home.vue
@@ -1,12 +1,14 @@
 <template>
-  <PrivilegedHome v-if="isPrivilegedUser"/>
-  <NonPrivilegedHome v-if="loaded && authStore.isLoaded && !isPrivilegedUser"/>
+  <template v-if="loaded">
+    <PrivilegedHome v-if="isPrivilegedUser && (!appConfig.homeScreen || appConfig.homeScreen === '/')"/>
+    <NonPrivilegedHome v-else/>
+  </template>
 </template>
 <script setup lang="ts">
-import { isPrivilegedUser, authStore } from '@/modules/core/store/auth';
+import { isPrivilegedUser } from '@/modules/core/store/auth';
 import PrivilegedHome from '@/modules/core/PrivilegedHome.vue';
 import NonPrivilegedHome from '@/modules/core/NonPrivilegedHome.vue';
 import { useAppConfiguration } from '@/modules/configurations/store/app-configuration';
 
-const { loaded } = useAppConfiguration();
+const { loaded, appConfig } = useAppConfiguration();
 </script>
