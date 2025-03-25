90f4e9fac05f612baccfed5547ac0e92728cd0aa
David Meir-Levy <david@davidlevy.co.il>
2025-03-25 22:49:27 +0200

    fix gcs auth file

--- apps/admin/src/modules/assets/components/StorageGcsAuth.vue
+++ apps/admin/src/modules/assets/components/StorageGcsAuth.vue
@@ -1,58 +1,57 @@
 <template>
-	<div>
-		<FormInput title="Select GCS JSON Key" type="file" v-model="file" @change.native="setFileData"/>
-	</div>
+	<form-input-item :label="$t('GCS JSON Key')">
+		<el-input type="file" v-model="file" @change.native="setFileData" />
+	</form-input-item>
 </template>
-<script lang="ts">
-import {watchEffect, reactive, ref} from 'vue'
-import FormInput from '../../core/components/forms/FormInput.vue'
+<script lang="ts" setup>
+import { watchEffect, reactive, ref } from 'vue'
 
-export default {
-	name: 'StorageGcsAuth',
-	components: {FormInput},
-	setup(props, {emit}) {
-		const form = reactive({
-			projectId: '',
-			clientEmail: '',
-			privateKey: '',
-		})
+const props = defineProps({value: Object})
+const emit = defineEmits(['update:modelValue'])
 
-		function getFormInputs({projectId, clientEmail, privateKey}) {
-			return {projectId, clientEmail, privateKey};
+const form = reactive({
+	projectId: '',
+	clientEmail: '',
+	privateKey: '',
+})
+
+const file = ref(null)
+
+if (props.value) {
+	form.projectId = props.value.projectId
+	form.clientEmail = props.value.clientEmail
+	form.privateKey = props.value.privateKey
+}
+
+function getFormInputs({ projectId, clientEmail, privateKey }) {
+	return { projectId, clientEmail, privateKey };
+}
+
+watchEffect(() => emit('update:modelValue', getFormInputs(form)))
+
+function readFile(file: File): Promise<string> {
+	return new Promise((resolve, reject) => {
+		const reader = new FileReader()
+		reader.readAsText(file, 'UTF-8')
+		reader.onload = function (evt: any) {
+			resolve(evt?.target.result)
 		}
-
-		watchEffect(() => emit('update:modelValue', getFormInputs(form)))
-
-		function readFile(file: File): Promise<string> {
-			return new Promise((resolve, reject) => {
-				const reader = new FileReader()
-				reader.readAsText(file, 'UTF-8')
-				reader.onload = function (evt: any) {
-					resolve(evt?.target.result)
-				}
-				reader.onerror = function () {
-					reject(new Error('error reading file'))
-				}
-			})
+		reader.onerror = function () {
+			reject(new Error('error reading file'))
 		}
+	})
+}
+async function setFileData($event) {
+	if ($event.target.files && $event.target.files[0]) {
+		const {
+			project_id: projectId,
+			private_key: privateKey,
+			client_email: clientEmail
+		} = JSON.parse(await readFile($event.target.files[0]))
 
-		return {
-			form,
-			file: ref(null),
-			async setFileData($event) {
-				if ($event.target.files && $event.target.files[0]) {
-					const {
-						project_id: projectId,
-						private_key: privateKey,
-						client_email: clientEmail
-					} = JSON.parse(await readFile($event.target.files[0]))
-
-					form.projectId = projectId
-					form.privateKey = privateKey
-					form.clientEmail = clientEmail
-				}
-			}
-		}
+		form.projectId = projectId
+		form.privateKey = privateKey
+		form.clientEmail = clientEmail
 	}
 }
 </script>
