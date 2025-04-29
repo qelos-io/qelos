23facce421036616e402f4dbd38d9bc3552d40b1
David Meir-Levy <david@davidlevy.co.il>
2025-04-29 13:19:16 +0300

    navigate to edited plugin after create

--- apps/admin/src/modules/plugins/CreatePlugin.vue
+++ apps/admin/src/modules/plugins/CreatePlugin.vue
@@ -4,15 +4,20 @@
   </div>
 </template>
 <script lang="ts" setup>
+import { useRouter } from 'vue-router';
 import PluginForm from './components/PluginForm.vue';
 import { useCreatePlugin } from './compositions/manage-plugin';
 import { usePluginsList } from '@/modules/plugins/store/plugins-list';
+import { IPlugin } from '@qelos/global-types';
+
+const router = useRouter();
 
 const { plugin, submitting, savePlugin } = useCreatePlugin();
 const store = usePluginsList()
 
-const save = async (data) => {
-  await savePlugin(data)
+const save = async (data: Partial<IPlugin>) => {
+  const plugin = await savePlugin(data)
   store.retry();
+  router.push({ name: 'editPlugin', params: { pluginId: plugin._id } });
 }
 </script>
