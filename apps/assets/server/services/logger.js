f39edaf1d5b73dae8fc8cd0e21125e776c2cc7b9
David Meir-Levy <david@davidlevy.co.il>
2025-08-22 05:54:40 +0300

    feat: add warn and info logging methods to logger service

--- apps/assets/server/services/logger.js
+++ apps/assets/server/services/logger.js
@@ -2,11 +2,15 @@ const {showLogs} = require('../../config');
 
 const logger = {
   log: ((...args) => undefined),
-  error: ((...args) => undefined)
+  warn: ((...args) => undefined),
+  error: ((...args) => undefined),
+  info: ((...args) => undefined)
 }
 
 if (showLogs) {
   logger.log = console.log;
+  logger.warn = console.warn;
+  logger.info = console.info;
   logger.error = console.error;
 }
 
