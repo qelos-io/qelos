0bc16ea4d829c4d1e6237ef3b14e5d3f44386238
David Meir-Levy <david@davidlevy.co.il>
2026-02-10 14:05:53 +0200

    dev gateway message

--- apps/gateway/index.ts
+++ apps/gateway/index.ts
@@ -13,6 +13,14 @@ start("Gateway", port, address)
   .finally(() => {
     if (process.env.NODE_ENV !== 'production') {
       fetch(`http://${address}:${port}`)
-        .then(res => res.status < 500 ? console.log('Gateway is running on dev') : process.exit(1));
+        .then(res => res.status < 500 ? console.log('Gateway is running on dev') : process.exit(1))
+        .then(require('timers/promises').setTimeout(3000))
+        .then(() => {
+          console.log('------'); 
+          console.log('\n\x1b[32m🚀 Qelos Gateway is running!\x1b[0m');
+          console.log('\x1b[36m📍 Admin UI: \x1b[4mhttp://localhost:3000\x1b[0m');
+          console.log('\x1b[33m📚 API Documentation: \x1b[4mhttps://docs.qelos.io\x1b[0m');
+          console.log('------'); 
+        })
     }
   });
